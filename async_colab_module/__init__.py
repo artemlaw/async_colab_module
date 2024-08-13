abbbb0e439b5032f1e1ba3738fab250305791730
Artem Lubentsov <artem.law@mail.ru>
2024-08-13 22:42:40 +0300

    Add display_form

--- async_colab_module/__init__.py
+++ async_colab_module/__init__.py
@@ -2,4 +2,4 @@ from .moysklad import MoySklad
 from .wb import WB
 # from .tabstyle import TabStyles
 from .utils import *
-from display_form import get_display_form, ProgressBar
+from .display_form import get_display_form, ProgressBar
